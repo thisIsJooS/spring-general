4283c865394a70db12c3b4a70cf03fbf477ae36d
thisIsJooS <thisis.joos@gmail.com>
2023-01-17 21:54:44 +0900

    update api posts

--- src/main/java/com/general/gen/service/posts/PostsService.java
+++ src/main/java/com/general/gen/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.general.gen.service.posts;
 
+import com.general.gen.domain.posts.Posts;
 import com.general.gen.domain.posts.PostsRepository;
+import com.general.gen.web.Dto.PostsResponseDto;
 import com.general.gen.web.Dto.PostsSaveRequestDto;
+import com.general.gen.web.Dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,27 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+
+    /**
+     * 데이터베이스에 쿼리를 날리는 부분이 없다.
+     * 이게 가능한 이유는 JPA의 영속성 컨텍스트 때문이다.
+     * 영속성 컨텍스트란, 엔티티를 영구 저장하는 환경이다.
+     */
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
