f12e956650e5d65d9e7b1f742fc08b35c9484bff
thisIsJooS <thisis.joos@gmail.com>
2023-01-17 22:36:00 +0900

    BaseTimeEntity test

--- src/test/java/com/general/gen/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/general/gen/domain/posts/PostsRepositoryTest.java
@@ -3,12 +3,14 @@ package com.general.gen.domain.posts;
 import org.aspectj.lang.annotation.After;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -47,4 +49,27 @@ class PostsRepositoryTest {
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
     }
+
+    @Test
+    @DisplayName("BaseTimeEntity를 등록한다.")
+    public void dasd() throws Exception{
+        //given
+        LocalDateTime now = LocalDateTime.of(2023, 1, 17, 22, 33, 0);
+        postsRepository.save(Posts.builder()
+                        .title("title")
+                        .content("content")
+                        .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+
 }
