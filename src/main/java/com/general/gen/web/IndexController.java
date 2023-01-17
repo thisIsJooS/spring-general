a67fd1cbe75eae36840c74e89fed9f841b12222d
thisIsJooS <thisis.joos@gmail.com>
2023-01-18 01:09:52 +0900

    게시글 수정

--- src/main/java/com/general/gen/web/IndexController.java
+++ src/main/java/com/general/gen/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.general.gen.web;
 
 import com.general.gen.service.posts.PostsService;
+import com.general.gen.web.Dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,5 +22,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
