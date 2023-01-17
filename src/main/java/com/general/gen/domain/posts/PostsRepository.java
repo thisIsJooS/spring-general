79bd14282230ba4eb2703425adc64ebc3edcc177
thisIsJooS <thisis.joos@gmail.com>
2023-01-18 01:04:36 +0900

    게시글 등록, 조회

--- src/main/java/com/general/gen/domain/posts/PostsRepository.java
+++ src/main/java/com/general/gen/domain/posts/PostsRepository.java
@@ -1,8 +1,17 @@
 package com.general.gen.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    // SpringDataJpa에서 제공하지 않는 메서드는 아래처럼 쿼리로 작성해도 된다.
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+
+
 }
--- src/main/java/com/general/gen/service/posts/PostsService.java
+++ src/main/java/com/general/gen/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.general.gen.service.posts;
 
 import com.general.gen.domain.posts.Posts;
 import com.general.gen.domain.posts.PostsRepository;
+import com.general.gen.web.Dto.PostsListResponseDto;
 import com.general.gen.web.Dto.PostsResponseDto;
 import com.general.gen.web.Dto.PostsSaveRequestDto;
 import com.general.gen.web.Dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -39,5 +43,21 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    /**
+     * readOnly = true 옵션을 주면,
+     * 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선되기 때문에,
+     * 등록, 수정, 삭제 기능이 전혀 없는 서비스 메서드에서 사용하는 것을 추천한다.
+     * <p>
+     * .map(PostsListResponseDto::new)
+     * =
+     * .map(posts -> new PostsListResponseDto(posts))
+     */
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
